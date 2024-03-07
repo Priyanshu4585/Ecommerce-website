5f7dcaa721dff7e1c6c63875ccf94a0d26a64ad2
Priyanshu Singh <152967318+Priyanshu4585@users.noreply.github.com>
2024-03-07 21:34:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+let cartN = 0;
 const bar = document.getElementById("bar")
 const close = document.getElementById("close")
 const nav = document.getElementById("navbar")
+const cartNo = document.getElementById("cartNo")
+const cartNo1 = document.getElementById("cartNo1")
+const cart1 = document.getElementById("cart1")
+const cart2 = document.getElementById("cart2")
+const cart3 = document.getElementById("cart3")
+const cart4 = document.getElementById("cart4")
+const cart5 = document.getElementById("cart5")
+const cart6 = document.getElementById("cart6")
+const cart7 = document.getElementById("cart7")
+const cart8 = document.getElementById("cart8")
+const cart9 = document.getElementById("cart9")
+const cart10 = document.getElementById("cart10")
+const cart11 = document.getElementById("cart11")
+const cart12 = document.getElementById("cart12")
+const cart13 = document.getElementById("cart13")
+const cart14 = document.getElementById("cart14")
+const cart15 = document.getElementById("cart15")
+const cart16 = document.getElementById("cart16")
 
 if(bar){
     bar.addEventListener("click",()=>{
@@ -15,3 +34,134 @@ if(close){
 
     })
 }
+
+
+if(cart1){
+  cart1.addEventListener("click",()=>
+{       
+cartN ++;
+cartNo.innerText = cartN ;
+cartNo1.innerText = cartN ;
+})
+}
+
+if(cart2){
+    cart2.addEventListener("click",()=>
+  {       
+  cartN ++;
+  cartNo.innerText = cartN ;
+  cartNo1.innerText = cartN ;
+  })
+  }
+  if(cart3){
+    cart3.addEventListener("click",()=>
+  {       
+  cartN ++;
+  cartNo.innerText = cartN ;
+  cartNo1.innerText = cartN ;
+  })
+  }
+  if(cart4){
+    cart4.addEventListener("click",()=>
+  {       
+  cartN ++;
+  cartNo.innerText = cartN ;
+  cartNo1.innerText = cartN ;
+  })
+  }
+  if(cart5){
+    cart5.addEventListener("click",()=>
+  {       
+  cartN ++;
+  cartNo.innerText = cartN ;
+  cartNo1.innerText = cartN ;
+  })
+  }
+  if(cart6){
+    cart6.addEventListener("click",()=>
+  {       
+  cartN ++;
+  cartNo.innerText = cartN ;
+  cartNo1.innerText = cartN ;
+  })
+  }
+  if(cart7){
+    cart7.addEventListener("click",()=>
+  {       
+  cartN ++;
+  cartNo.innerText = cartN ;
+  cartNo1.innerText = cartN ;
+  })
+  }
+  if(cart8){
+    cart8.addEventListener("click",()=>
+  {       
+  cartN ++;
+  cartNo.innerText = cartN ;
+  cartNo1.innerText = cartN ;
+  })
+  }
+  if(cart9){
+    cart9.addEventListener("click",()=>
+  {       
+  cartN ++;
+  cartNo.innerText = cartN ;
+  cartNo1.innerText = cartN ;
+  })
+  }
+  if(cart10){
+    cart10.addEventListener("click",()=>
+  {       
+  cartN ++;
+  cartNo.innerText = cartN ;
+  cartNo1.innerText = cartN ;
+  })
+  }
+  if(cart11){
+    cart11.addEventListener("click",()=>
+  {       
+  cartN ++;
+  cartNo.innerText = cartN ;
+  cartNo1.innerText = cartN ;
+  })
+  }
+  if(cart12){
+    cart12.addEventListener("click",()=>
+  {       
+  cartN ++;
+  cartNo.innerText = cartN ;
+  cartNo1.innerText = cartN ;
+  })
+  }
+  if(cart13){
+    cart13.addEventListener("click",()=>
+  {       
+  cartN ++;
+  cartNo.innerText = cartN ;
+  cartNo1.innerText = cartN ;
+  })
+  }
+  if(cart14){
+    cart14.addEventListener("click",()=>
+  {       
+  cartN ++;
+  cartNo.innerText = cartN ;
+  cartNo1.innerText = cartN ;
+  })
+  }
+  if(cart15){
+    cart15.addEventListener("click",()=>
+  {       
+  cartN ++;
+  cartNo.innerText = cartN ;
+  cartNo1.innerText = cartN ;
+  })
+  }
+  if(cart16){
+    cart16.addEventListener("click",()=>
+  {       
+  cartN ++;
+  cartNo.innerText = cartN ;
+  cartNo1.innerText = cartN ;
+  })
+  }
